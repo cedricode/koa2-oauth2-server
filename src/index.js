8ee71399f13c2b1bc936cd8eaacbf9990b6daece
Elegode <cedric.yang@ihealthlabs.eu>
2018-01-26 08:32:24 +0100

    password grant

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // @flow
+
 var Koa = require('koa');
-var bodyParser = require('koa-bodyparser');
+var bodyParser = require('koa-body');
 var OAuth2Server = require('oauth2-server');
+
+var Model = require('./model');
 
 var KoaRouter = require('koa-router');
 
@@ -12,26 +15,84 @@ class KoaOAuth2Server {
   }
 
   authenticateMiddleware() {
+    const self = this;
     return function authenticate(ctx, next) {
       const request = new OAuth2Server.Request(ctx.request);
       const response = new OAuth2Server.Response(ctx.response);
-      return this.OAuth2Server.authenticate(request, response);
+      return self.OAuth2Server.authenticate(request, response);
+    }
+  }
+
+  authorizeMiddleware(options) {
+    const self = this;
+    return async function authorize(ctx, next) {
+      const request = new OAuth2Server.Request(ctx.request);
+      const response = new OAuth2Server.Response(ctx.response);
+      const code = await self.OAuth2Server.authorize(request, response, options);  
+      ctx.code = code;
+      next();
     }
   }
 
   token() {
+    const self = this;
     return async function token(ctx, next) {
       const request = new OAuth2Server.Request(ctx.request);
       const response = new OAuth2Server.Response(ctx.response);
-      return this.OAuth2Server.token(request, response);
+      const newToken = await self.OAuth2Server.token(request, response);
+      ctx.token = newToken;
+      next();
     }
   }
 }
 
-const koaOAuth2Server = new KoaOAuth2Server({model: TODO:});
+const koaOAuth2Server = new KoaOAuth2Server({model: Model});
 
 const router = new KoaRouter();
 
-router.post('/oauth2/token', koaOAuth2Server.token());
+const app = new Koa();
 
-router.post('/oauth2/')
+app.use(bodyParser({
+  urlencoded: true
+}));
+
+router.post('/oauth2/token', koaOAuth2Server.token(), (ctx, next) => {
+  if (!ctx.token) {
+    ctx.status = 400;
+    ctx.body = 'Now Allowed';
+  } else {
+    ctx.body = ctx.token;
+    ctx.status = 200;
+  }
+});
+
+router.post('/oauth2/authorize',
+  koaOAuth2Server.authorizeMiddleware({
+    authenticateHandler: {
+      handle(request, response) {
+        return {
+          username: 3,
+          password: 4
+        }
+      }
+    }}), (ctx, next) => {
+  ctx.body = ctx.code;
+})
+
+router.post('/oauth2/');
+
+router.get('/hello', (ctx) => {
+  ctx.status = 200;
+  ctx.body = 'hello\n';
+});
+
+
+app.use(router.routes());
+
+app.listen(3000, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('app is listening on  port 3000...');
+});
+
--- src/model.js
+++ src/model.js
@@ -1,5 +1,42 @@
+import type { AuthorizationCode } from '@types/oauth2-server';
+
+// @flow
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var uuid = require('uuid');
+
+class Client {
+  clientId: string;
+  clientSecret: string;
+  redirectUris: ?string[];
+  constructor(clientId: string, clientSecret: string, redirectUris: ?string[]) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.redirectUris = redirectUris;
+  }
+}
+
+class Token {
+  clientId: string;
+  refreshToken: string;
+  refreshTokenExpiresAt: number;
+  userId: string;
+  constructor(clientId, refreshToken, refreshTokenExpiresAt, userId) {
+    Object.assign(this, {clientId, refreshToken, refreshTokenExpiresAt, userId});
+  }
+}
+
+class User {
+  email: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  username: string;
+  constructor(email, firstname, lastname, password, username) {
+    Object.assign(this, {email, firstname, lastname, password, username});
+  }
+}
 
 mongoose.model('OAuthTokens', new Schema({
   clientId: { type: String },
@@ -28,30 +65,85 @@ var OAuthTokensModel = mongoose.model('OAuthTokens');
 var OAuthClientsModel = mongoose.model('OAuthClients');
 var OAuthUsersModel = mongoose.model('OAuthUsers');
 
-module.exports.getAccessToken = function (bearerToken) {
+const TokenTable = {};
+const AuthorizationCodeTable = {};
+
+module.exports.getAccessToken = function (bearerToken:string) {
   //TODO:  JWT validation.
 }
 
-module.exports.getClient = function(clientId, clientSecret) {
-  // TODO: return applibrary
+module.exports.getClient = function(clientId:string, clientSecret:string) {
+  console.log('client credentials');
+  return {
+    id: 'someid',
+    clientId,
+    clientSecret,
+    name: 'Cedric-Company',
+    grants: ['password', 'authorization_code'],
+    accessTokenLifeTime: 15 * 60,
+    refreshTokenLifeTime: 30 * 24 * 60 * 60,
+    redirectUris: ['google.com']
+  };
 }
 
-module.exports.getRefreshToken = function (refreshToken)  {
+module.exports.getRefreshToken = function (refreshToken:string)  {
+  console.log('refresh token');
   return OAuthTokensModel.findOne({refreshToken});
 }
 
-module.exports.getUser = function (username, password) {
-  return OAuthUsersModel.find({username, password});
+module.exports.getUser = function (username:string, password:string) {
+  console.log('get user');
+  return {username, password};
+  // return OAuthUsersModel.find({username, password});
 }
 
-module.exports.saveToken = function (token, client, uesr) {
-  return OAuthTokensModel.save({clientId: client._id, refreshToken, refreshTokenExpiresOn, userId: user._id});
+module.exports.saveToken = function (token:string, client: Client, user: User) {
+  console.log('save token');
+  TokenTable[client.clientId+user.username] = {
+    accessToken: uuid(),
+    accessTokenExpiresAt: datePlusMill(new Date(), 60 * 24 * 60 * 60 * 1000),
+    refreshToken: uuid(),
+    refreshTokenExpiresAt: datePlusMill(new Date(), 15 * 60),
+    client: client,
+    user
+  };
+  return TokenTable[client.clientId+user.username];
+  // return OAuthTokensModel.save({clientId: client._id, refreshToken, refreshTokenExpiresOn, userId: user._id});
 }
 
-module.exports.revokeToken = function (token) {
+module.exports.revokeToken = function (token: string) {
+  console.log('revoke token');
   return OAuthTokensModel.remove({refreshToken: token});
 }
 
+module.exports.getAuthorizationCode = function (code: string) {
+  return AuthorizationCodeTable[code];
+}
+
+module.exports.saveAuthorizationCode = function (code: string, client: Client, user: User) {
+  AuthorizationCodeTable[code] = {
+    authorizationCode: code,
+    code,
+    expiresAt: datePlusMill(new Date(), 60 * 1000),
+    redirectUri: 'www.google.com',
+    client: {id: 'someid'},
+    user: {
+      username: 3,
+      password: 4
+    }
+  };
+  return AuthorizationCodeTable[code];
+}
+
+module.exports.revokeAuthorizationCode = function (code: AuthorizationCode) {
+  delete AuthorizationCodeTable[code];
+  return true;
+}
+
+
+function datePlusMill(date, mills) {
+  return new Date(date.getTime() + mills);
+}
 
 
 
